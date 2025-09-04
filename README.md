a73f219033040a801f10d0f294e4017581e67539
Stewart Smith <stewdio@users.noreply.github.com>
2025-09-04 17:06:46 -0400

    VideoScrubber and StickyEase updates. Improved safety.

--- README.md
+++ README.md
@@ -7,3 +7,64 @@ Scrolling you from A to B since 2025.
 
 
 
+<!--
+3 different sizes, 
+x 16 different methods.
+
+
+
+SIZES (3):
+
+1. element.height  <  viewport.height
+2. element.height === viewport.height
+3. element.height  >  viewport.height
+
+
+THRESHOLDS (4):
+
+   VE = Viewport, Element
+1. 00 = V bottom, E bottom
+2. 01 = V bottom, E top
+3. 10 = V top, E bottom
+4. 11 = V top, E top
+
+
+METHODS (16):
+
+AB,XY
+if AB==XY then duration is 0 and we can skip all updates.
+if duration > 0, all good.
+if duration < 0 ... we need to recalculate!
+
+
+ 1. 00,00  duration === 0
+ 2. 00,01  INVERSION!! is backward!!! (but animation happens within viewport if rendered backward)
+ 3. 00,10  a normal method
+ 4. 00,11 “CONTAIN MIN” +++++++ e.height < v.height, is ok. if e==v, duration 0! if e>h INVERSION!!!!
+
+ 5. 01,00  a normal method.
+ 6. 01,01  duration === 0
+ 7. 01,10  a normal method. longest one! “COVER” (animation occurs when any bit of the element is visible at all.)
+ 8. 01,11  a normal method. 
+
+ 9. 10,00  INVERSION!! 
+10. 10,01  INVERSION!!
+11. 10,10  duration === 0
+12. 10,11  INVERSION!!
+
+13. 11,00 “CONTAIN MAX” +++++++ if e.height > v.height, is ok. if e==v duration 0! if e<h INVERSION!!
+14. 11,01  INVERSION!!
+15. 11,10  a normal method.
+16. 11,11  duration === 0
+
+
+
+
+1. if AB === XY, duration === 0 so skip calculating anything.
+
+2. To handle any inversion: swap AB with XY.
+
+3. For 00,11 and 11,00 need to know e.height vs v.height.
+		depending on method, might need to invert (swap AB with XY)
+
+-->
--- ScrollPacer.js
+++ ScrollPacer.js
@@ -1,3 +1,4 @@
+
 //  Copyright ©️ 2025 Stewart Smith. See LICENSE for details.
 
 
@@ -5,12 +6,49 @@
 
 import {
 
+	isNotUsefulBoolean,
 	isUsefulNumber,
+	isNotUsefulNumber,
 	isUsefulString,
 	normalize
 
 } from 'shoes-js'
 import Pacer from 'pacer-js'
+
+
+
+
+
+
+function methodToCode( e ){
+
+	return (
+		
+		''+
+		Number( e.beginOnViewportEdge === 'top' ) + 
+		Number( e.beginOnElementEdge === 'top' ) +
+		Number( e.endOnViewportEdge === 'top' ) +
+		Number( e.endOnElementEdge === 'top' )
+	)
+}
+function codeToMethod( e ){
+
+	return {
+
+		beginOnViewportEdge: Number( e[ 0 ]) ? 'top' : 'bottom',
+		beginOnElementEdge: Number( e[ 1 ]) ? 'top' : 'bottom',
+		endOnViewportEdge: Number( e[ 2 ]) ? 'top' : 'bottom',
+		endOnElementEdge: Number( e[ 3 ]) ? 'top' : 'bottom'
+	}
+}
+const inversions = [
+	
+	'0001',
+	'1001',
+	'1001',
+	'1011',
+	'1101'
+]
 
 
 
@@ -28,7 +66,7 @@ class ScrollPacer extends Pacer {
 
 		if( typeof watchedEl === 'string' ) watchedEl = document.getElementById( watchedEl )
 		if( watchedEl instanceof Element !== true ) watchedEl = document.body
-		super( label )
+		super( label, 'n' )
 		this.watchedEl = watchedEl
 
 		if( typeof params !== 'object' ) params = {}
@@ -96,9 +134,66 @@ class ScrollPacer extends Pacer {
 			? params.endOffsetRelative
 			: 0
 	}
-	update(){
+	update( n ){
+
+
+		//  Perhaps we’d like to force a particular scroll state?
+		//  We’re expecting a value in the range 0..1,
+		//  though we can handle any useful numeric value.
+
+		if( isUsefulNumber( n )){
+
+			super.update( n )
+			return this
+		}
+
+
+		//  Otherwise, let’s figure out where we are scroll-wise.
 
 		const rect = this.watchedEl.getBoundingClientRect()
+
+
+
+
+		const code = methodToCode( this )
+		
+		let 
+		durationIsZero = false,
+		requiresInversion = false
+
+		if( code.substring( 0, 2 ) === code.substring( 2, 4 )){
+
+			durationIsZero = true
+		}
+		else if( inversions.includes( code )){
+
+			requiresInversion = true
+		}
+		else if( code === '0011' || code === '1100' ){
+
+			if( rect.height === window.innerHeight ){
+				
+				durationIsZero = true
+			}
+			else if( 
+				( code === '0011' && rect.height > window.innerHeight ) ||
+				( code === '1100' && rect.height < window.innerHeight )){
+					
+				requiresInversion = true
+			}
+		}
+		if( durationIsZero ){
+
+			//++++  Should probably fire an onKey(). Come back to this idea.
+			return this
+		}
+		else if( requiresInversion ){
+
+			const inverted = code.substring( 2, 4 ) + code.substring( 0, 2 )
+			Object.assign( this, codeToMethod( inverted ))
+		}
+
+
 
 
 		//  n = 0 occurs when the top edge of our target element
@@ -125,7 +220,6 @@ class ScrollPacer extends Pacer {
 		}
 		else if( this.endOnViewportEdge === 'bottom' ){
 
-			// endEdge = window.innerHeight - rect[ this.endOnElementEdge ]
 			endEdge = rect[ this.endOnElementEdge ] - window.innerHeight
 		}
 
@@ -147,67 +241,119 @@ class ScrollPacer extends Pacer {
 		//  Mark it zero.
 		// (Its a league game, Smokey.)
 
-		const n = normalize( 
+		n = normalize( 
 			
 			0, 
-			beginEdge,
-			endEdge
+			Math.min( beginEdge, endEdge ),
+			Math.max( beginEdge, endEdge )
 		)
+		this.scrollN = n
 		super.update( n )
 	}
 
 
-
 	//   Helpers.
 
-	static makeStickySoftener( 
-		
-		containerEl,
-		targetEl, 
-		softStickyBuffer, 
-		label ){
+	static stickyEaseBuffer = 200
+	static stickyEaseUpdate( e, p ){
 
-		if( isNotUsefulNumber( softStickyBuffer )){
+		p.targetEl.style.translate = `0 ${ e.y }px`
+	}
+	static makeStickyEaseStart( containerEl, targetEl, buffer, label ){
+
+		if( isNotUsefulNumber( buffer )){
 			
-			//  Don’t forget to mirror this value in the CSS: var( --soft-sticky-buffer );
-			softStickyBuffer = 200
+			//  Mirror this value in the CSS: var( --sticky-ease-buffer );
+			buffer = ScrollPacer.stickyEaseBuffer
 		}
-		targetEl.style.translate = `0 -${ softStickyBuffer }px`
-		function softenerUpdater( e, p ){
-
-			p.targetEl.style.translate = `0 ${ e.y }px`
-		}
-		const p = new ScrollPacer( 
+		return new ScrollPacer( 
 			
 			containerEl,
 			{
 				targetEl: targetEl,
+				beginOnViewportEdge: 'top',
 				beginOnElementEdge:  'top',
-				beginOnViewportEdge: 'bottom',
-				endOnElementEdge:    'top',
 				endOnViewportEdge:   'top',
-				endOffsetAbsolute:    softStickyBuffer,
+				endOnElementEdge:    'bottom',
+				// endOffsetAbsolute:    buffer,
 			},
 			label
 		)
-		.abs( 0.0, { y: softStickyBuffer * -1 })
-		.tween( ScrollPacer.quintic.in )
-		.abs( 1.0, { y: 0 })
-		.onEveryKey( softenerUpdater )
-		.onEveryTween( softenerUpdater )
-
-		return p
+		.abs( 0.0, { y: 0 })
+		.tween( ScrollPacer.cubic.in )
+		.abs( 1.0, { y: buffer })
+		.onEveryKey( stickyEaseUpdate )
+		.onEveryTween( stickyEaseUpdate )
 	}
-	static videoScrubber( e, p ){
+	static makeStickyEaseStop( containerEl, targetEl, buffer, label ){
 
+		if( isNotUsefulNumber( buffer )){
+			
+			//  Mirror this value in the CSS: var( --sticky-ease-buffer );
+			buffer = ScrollPacer.stickyEaseBuffer
+		}
+		return new ScrollPacer( 
+			
+			containerEl,
+			{
+				targetEl: targetEl,
+				beginOnViewportEdge: 'bottom',
+				beginOnElementEdge:  'top',
+				endOnViewportEdge:   'top',
+				endOnElementEdge:    'top'
+				// endOffsetAbsolute:    buffer,
+			},
+			label
+		)
+		.abs( 0.0, { y: -buffer })
+		.tween( ScrollPacer.cubic.in )
+		.abs( 1.0, { y: 0 })
+		.onEveryKey( stickyEaseUpdate )
+		.onEveryTween( stickyEaseUpdate )
+	}
+
+
+
+
+	static videoScrubberForce( e, p ){
+
+		ScrollPacer.videoScrubber( e, p, true )
+	}
+	static videoScrubberBegin( e, p ){
+
+		ScrollPacer.videoScrubber({ n: 0 }, p, true )
+	}
+	static videoScrubberEnd( e, p ){
+		
+		ScrollPacer.videoScrubber({ n: 1 }, p, true )
+	}
+	static videoScrubber( e, p, shouldForce ){
+		
 		if( p.targetEl instanceof Element !== true ||
 			isNotUsefulNumber( p.targetEl.duration ) ||
 			isNotUsefulNumber( p.targetEl.currentTime )){
 
 			return
 		}
-		const targetTime = p.targetEl.duration * e.n
-		p.targetEl.currentTime = ''+ targetTime
+		if( isNotUsefulNumber( p.lastSeekIssuedAt )){
+
+			p.lastSeekIssuedAt = 0
+		}
+		if( isNotUsefulBoolean( p.targetEl.isSeekReady )){
+
+			p.targetEl.isSeekReady = true
+			p.targetEl.addEventListener( 'seeked', function(){
+
+				p.targetEl.isSeekReady = true
+			})
+		}
+		const timeDiff = Date.now() - p.lastSeekIssuedAt
+		if( shouldForce || ( timeDiff > 16 && p.targetEl.isSeekReady )){
+
+			p.targetEl.currentTime = ''+ p.targetEl.duration * e.n
+			p.lastSeekIssuedAt = Date.now()
+			p.targetEl.isSeekReady = false
+		}
 	}
 }
 
